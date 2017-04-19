120e55f1f2334be5d46d09a3516a81020b6c3c68
Muhammad Alam <alam@mmtutor.com>
2017-04-19 17:05:13 -0400

    add onEnter hooks for logged in users

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
-import SessionFormContainer from './session_form/session_form_container';
 import App from './app';
+import SessionFormContainer from './session_form/session_form_container';
 
-const Root = () => {
+
+
+const Root = ({ store }) => {
+
+  const _redirectIfLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (currentUser) {
+      replace('/');
+    }
+  }
+
   return(
     <Provider store={ store }>
       <Router history={hashHistory} >
         <Route path="/" component={App}>
-          <Route path="/login" component={SessionFormContainer} />
-          <Route path="/signup" component={SessionFormContainer} />
+          <Route path="/login" component={SessionFormContainer} onEnter={_redirectIfLoggedIn} />
+          <Route path="/signup" component={SessionFormContainer} onEnter={_redirectIfLoggedIn} />
         </Route>
       </Router>
     </Provider>
