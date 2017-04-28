e69a4acd749d1b0c487933047b12d7b5ee07d9da
M M Alam <alam@mmtutor.com>
2017-04-28 14:22:26 -0400

    fix styles up a little bit

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -3,40 +3,13 @@ import { Link } from 'react-router';
 
 import TruckMap from '../truck_map/truck_map';
 
-
-// const handleSearchSubmit = (e) => {
-//   e.preventDefault();
-//   let addressString = e.currentTarget.firstChild.value;
-//   let mapCtrParams = {
-//     center: { lat: 40.7309907, lng: -73.8672127 },
-//     zoom: 21
-//   }
-//   debugger
-//   $.ajax(
-//     { method: 'get',
-//       url:'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBLGSUQ5XA6Ao6JhjrlAUG8K0EyL3UZIkI',
-//       data: {
-//         address: addressString
-//       }
-//     }
-//   ).then( result => {
-//     mapCtrParams.center.lat = result.results[0].geometry.location.lat;
-//     mapCtrParams.center.lng = result.results[0].geometry.location.lng;
-//     debugger
-//     return(
-//       <TruckMap mapOptions={mapCtrParams} />
-//     )
-//   })
-// }
-
-
 const navbarLeft = (handleSearchSubmit) => (
   <div className="navbar-left">
     <Link to="/">
       <img src={window.optimisedsvg} />
     </Link>
     <form onSubmit={handleSearchSubmit}>
-      <input type="search" placeholder="Search"/>
+      <input type="search" placeholder="Search by Location!"/>
       <button>
         <i className="fa fa-search" aria-hidden="true"></i>
       </button>
